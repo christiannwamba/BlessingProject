b12a5aebd8557c69598ce4997effba8e36b5d5cb
christiannwamba <nwambachristian@gmail.com>
2014-08-12 13:56:44 +0100

    Update IdentityModels.cs

--- Payroll/Payroll/Models/IdentityModels.cs
+++ Payroll/Payroll/Models/IdentityModels.cs
@@ -10,7 +10,7 @@ namespace PayrollProject.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("PayrollContext")
         {
         }
     }
